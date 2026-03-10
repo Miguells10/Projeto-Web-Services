60fb0b9f1e914b4600e452f12c8c183f34bf2081
Miguel Lucas <lucasmiguel646@gmail.com>
2026-03-10 15:43:36 -0300

    Set explicit JPA entity and service bean names
    Give the JPA entity an explicit name (ClientUser) and assign a specific Spring bean name (userEntityService) to UserService. These changes make the entity name explicit for JPA mapping and provide a stable bean id to avoid naming collisions or to allow injection by name.

--- src/main/java/com/mycompany/myapp/entities/User.java
+++ src/main/java/com/mycompany/myapp/entities/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 
-@Entity
+@Entity(name = "ClientUser")
 @Table(name = "tb_user")
 public class User implements Serializable {
 
--- src/main/java/com/mycompany/myapp/services/UserService.java
+++ src/main/java/com/mycompany/myapp/services/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("userEntityService")
 public class UserService {
 
     @Autowired
